1802128917867dbc12d547a5f0347e6a31e6713d
LI JUNJIE <zarekli@outlook.com>
2021-03-02 14:30:48 +0800

    3.2

--- Model/XNet.py
+++ Model/XNet.py
@@ -1,105 +1,39 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torchstat import stat
-from torchsummary import summary
+from Model.torch_utils import Conv2dStaticSamePadding, MaxPool2dStaticSamePadding
+
+# input = torch.randn(2, 2, 10, 10)
+# m = nn.Conv2d(2, 5, kernel_size=1, bias=False)
+# output = m(input)
+# print(output.shape)
 
 
-# GN使用，将6通道分成3组，一组2通道；实验证明一组16通道比较好；
-input = torch.randn(20, 6, 10, 10)
- m = nn.GroupNorm(3, 6)
- output = m(input)
+# nn.Conv2d(inplanes, inplanes, kernel_size=3, stride=2, padding=1, bias=False, groups=inplanes)  降采样
+# nn.Conv2d(inplanes, inplanes, kernel_size=1, bias=False)  1x1conv
+# nn.Conv2d(inplanes, inplanes, kernel_size=3, padding=1, dilation=1, bias=False) 不改变原尺寸
+
+# self.gn = nn.GroupNorm(out_channels/16, out_channels)
+
+# nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3)
+
+# just pointwise_conv applies bias, depthwise_conv has no bias.
+
+# nn.UpsamplingBilinear2d(scale_factor=2) 上采样
 
 
-class SELayer(nn.Module):
-    def __init__(self, channel, reduction=16):
-        super(SELayer, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Linear(channel, channel // reduction, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(channel // reduction, channel, bias=False),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
-        return x * y.expand_as(x)
-
-
-def conv3x3(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-
-
-class SEBasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None,
-                 *, reduction=16):
-        super(SEBasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes, 1)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.se = SELayer(planes, reduction)
-        self.downsample = downsample
-        self.stride = stride
+class conv(nn.Module):
+    def __init__(self, in_channels, out_channels):  # 保证gn中能整除16，实验证明一组16通道比较好
+        super(conv, self).__init__()
+        self.depthwise_conv = Conv2dStaticSamePadding(in_channels, in_channels, kernel_size=3, stride=1, groups=in_channels, bias=False)
+        self.pointwise_conv = Conv2dStaticSamePadding(in_channels, out_channels, kernel_size=1, stride=1)
+        self.gn = nn.GroupNorm(out_channels/16, out_channels)
+        self.gelu = nn.GELU()
 
     def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.se(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-        class SEBasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None,
-                 *, reduction=16):
-        super(SEBasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes, 1)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.se = SELayer(planes, reduction)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.se(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
+        x = self.depthwise_conv(x)
+        x = self.pointwise_conv(x)
+        x = self.gelu(self.gn(x))
+        return x
 
 
 class XNet(nn.Module):
@@ -108,7 +42,6 @@ class XNet(nn.Module):
         super(XNet, self).__init__()
 
     def forward(self, x):
-
         return
 
 # torchsummary
