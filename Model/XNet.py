85e67056c4d1f7de6719160c3327a3a6e4844314
LI JUNJIE <npucslijunjie@outlook.com>
2021-04-05 16:20:36 +0800

    4.5

--- Model/XNet.py
+++ Model/XNet.py
@@ -6,15 +6,10 @@ from torchsummary import summary
 from Model.torch_utils import Conv2dStaticSamePadding, MaxPool2dStaticSamePadding
 
 
-# input = torch.randn(2, 2, 10, 10)
-# m = nn.UpsamplingBilinear2d(scale_factor=2)
-# output = m(input)
-# print(output.shape)
+def _upsample_like(src, tar):
+    src = F.interpolate(src, size=tar.shape[2:], mode='bilinear', align_corners=True)
 
-
-# nn.Conv2d(inplanes, inplanes, kernel_size=3, stride=2, padding=1, bias=False, groups=inplanes)  降采样
-# nn.Conv2d(inplanes, inplanes, kernel_size=1, bias=False)  1x1conv
-# nn.Conv2d(inplanes, inplanes, kernel_size=3, padding=1, dilation=1, bias=False) 不改变原尺寸
+    return src
 
 
 class SELayer(nn.Module):
@@ -34,7 +29,7 @@ class SELayer(nn.Module):
         y = self.fc(y).view(b, c, 1, 1)
         return x * y.expand_as(x)
 
-    
+
 class conv(nn.Module):
     def __init__(self, in_channels, out_channels):  # 保证gn中能整除16，实验证明一组16通道比较好
         super(conv, self).__init__()
@@ -63,9 +58,10 @@ class BasicBlock(nn.Module):
         self.conv_1 = conv(1 * mid_channels, mid_channels)
         self.conv_2 = conv(2 * mid_channels, mid_channels)
         self.conv_3 = conv(3 * mid_channels, mid_channels)
+        self.se = SELayer(out_channels)
+        self.gelu = nn.GELU()
 
     def forward(self, x):
-        hx = x
         x = self.pointwise_conv_in2mid(x)
 
         p3_0 = self.conv_1(x)
@@ -93,6 +89,7 @@ class BasicBlock(nn.Module):
         p3_2 = self.conv_3(torch.cat((p3_0b, self.upsample(p4_1), self.upsample(p4_2)), 1))
 
         x = self.pointwise_conv_mid2out(p3_2)
+        x = self.se(x)
 
         return x
 
@@ -101,16 +98,99 @@ class XNet(nn.Module):
 
     def __init__(self, in_channels=3, out_channels=1):
         super(XNet, self).__init__()
+        self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
+        self.pooling = MaxPool2dStaticSamePadding(3, 2)
+
+        self.conv = conv(in_channels=in_channels, out_channels=32)
+
+        self.m11 = BasicBlock(32, 32, 64)
+        self.m12 = BasicBlock(64, 64, 128)
+        self.m13 = BasicBlock(128, 128, 256)
+        self.m14 = BasicBlock(256, 256, 512)
+        self.m15 = BasicBlock(512, 512, 512)
+
+        self.m24 = BasicBlock(1024, 512, 256)
+        self.m23 = BasicBlock(512, 256, 128)
+        self.m22 = BasicBlock(256, 128, 64)
+
+        self.m31 = BasicBlock(128, 64, 128)
+        self.m32 = BasicBlock(320, 160, 320)
+        self.m33 = BasicBlock(704, 352, 704)
+        self.m34 = BasicBlock(1472, 736, 1472)
+        self.m35 = BasicBlock(1984, 992, 1984)
+
+        self.conv1 = nn.Conv2d(128, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(320, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv3 = nn.Conv2d(704, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv4 = nn.Conv2d(1472, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv5 = nn.Conv2d(1984, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.fusion = nn.Conv2d(5, 1, kernel_size=3, stride=1, padding=1, bias=False)
 
     def forward(self, x):
-        return
+        conv1 = self.conv(x)
 
+        x11 = self.m11(conv1)
+        x11B = x11
+        print(x11.shape)
+
+        x12 = self.m12(self.pooling(x11))
+        x12A, x12B = x12, x12
+        print(x12.shape)
+
+        x13 = self.m13(self.pooling(x12))
+        x13A, x13B = x13, x13
+        print(x13.shape)
+        print(self.pooling(x13).shape)
+
+        x14 = self.m14(self.pooling(x13))
+        x14A, x14B = x14, x14
+        print(x14.shape)
+
+        x15 = self.m15(self.pooling(x14))
+        x15B = x15
+
+        x24 = self.m24(torch.cat((x14B, self.upsample(x15)), 1))
+        x24A = x24
+
+        x23 = self.m23(torch.cat((x13A, self.upsample(x24)), 1))
+        x23A = x23
+
+        x22 = self.m22(torch.cat((x12A, self.upsample(x23)), 1))
+        x22A = x22
+
+        x31 = self.m31(torch.cat((x11B, self.upsample(x22)), 1))
+
+        x32 = self.m32(torch.cat((x22A, x12B, self.pooling(x31)), 1))
+
+        x33 = self.m33(torch.cat((x23A, x13B, self.pooling(x32)), 1))
+
+        x34 = self.m34(torch.cat((x24A, x14B, self.pooling(x33)), 1))
+
+        x35 = self.m35(torch.cat((x15B, self.pooling(x34)), 1))
+
+        out1 = self.conv1(x31)
+
+        out2 = _upsample_like(self.conv2(x32), out1)
+
+        out3 = _upsample_like(self.conv2(x33), out1)
+
+        out4 = _upsample_like(self.conv2(x34), out1)
+
+        out5 = _upsample_like(self.conv2(x35), out1)
+
+        fusion = self.fusion(torch.cat((out1, out2, out3, out4, out5), 1))
+
+        return F.sigmoid(fusion)
+
+
+# Bug ,BasicBlock最小支持输入尺寸为80，所以得改XNet网络深度
 
 # torchsummary
-# model = BasicBlock(in_channels=64, mid_channels=32, out_channels=32)
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# model = model.to(device)
-# print(summary(model, (64, 320, 320)))
+model = BasicBlock(128, 128, 256)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+print(summary(model, (128, 80, 80)))
 
 # # torchstat
 # model = XNet()
